87db3098e6de4d77d9de1cb9e46a229045c8d6a0
Marko Sustic <msustic91@gmail.com>
2026-04-14 07:00:27 +0200

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Geist } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const inter = Inter({
   variable: "--font-inter",
@@ -22,7 +25,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html
         lang="en"
-        className={`${inter.variable} h-full antialiased`}
+        className={cn("h-full", "antialiased", inter.variable, "font-sans", geist.variable)}
       >
         <body className="min-h-full flex flex-col">{children}</body>
       </html>
